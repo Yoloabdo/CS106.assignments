31951020e354a5bb14bd6a28c78f8907b0b58f23
abdoelrhman <dhooom.da@gmail.com>
2013-02-16 06:26:44 +0200

    Update Assignment2/Hailstone.java
    finished 

--- Assignment2/Hailstone.java
+++ Assignment2/Hailstone.java
@@ -1,7 +1,7 @@
 /*
  * File: Hailstone.java
- * Name: 
- * Section Leader: 
+ * Name: Abdulrhman Eaita
+ * 
  * --------------------
  * This file is the starter file for the Hailstone problem.
  */
@@ -10,7 +10,36 @@ import acm.program.*;
 
 public class Hailstone extends ConsoleProgram {
 	public void run() {
-		/* You fill this in */
+		/*
+		 * 
+		 * the algorithm for it via wikipedia
+		 * 
+		 * 
+		 */
+		int hailstone = readInt("Enter a number: ");
+		int x = 0;
+		while (hailstone >= 0){
+			
+			if (hailstone % 2 == 0){
+				println(hailstone + " is even so i take half: " +
+						hailstone / 2);
+				hailstone = hailstone / 2;
+				x++;
+			}
+			else{
+				if (hailstone == 1){
+					break;
+				}
+				println(hailstone + " is odd, so i make 3n + 1: " +
+						3*hailstone + 1);
+				hailstone = 3*hailstone + 1;
+				x++;
+			}
+		
+		}
+		println("The process took "+ x + " to reach 1");
+
 	}
+	
 }
 
